402e2c212cbc63e1c1bb63cd6b649377d4c85d5a
Dmytro <dmitry.khandogy@gmail.com>
2023-04-11 19:36:09 +0200

    Begin. Header - done. Main slider - done. (Desktop)

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const mainSlider = new Swiper('.main_slider .slider', {
+        speed: 750,
+        slidesPerView: 1,
+        spaceBetween: 50,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+    });
+});
